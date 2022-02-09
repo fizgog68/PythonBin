079ab0e90cfa2e681aeb66727aa46e8d96f39995
fizzy <99197411+fizgog68@users.noreply.github.com>
2022-02-09 14:57:53 +0000

    Add files via upload

---
+++ foofightersmonkeywrench.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def what():
+    print("What have we dont with innocence?")
+    sleep(3)
+    print("It disappeared with time, it never made much sense.")
+    sleep(3)
+    print("Adolescent resident.")
+    sleep(3)
+    print("Wasting another night on planning my revenge")
+    sleep(3)
+    print("One in ten")
+    sleep(3)
+    print("One in ten")
+    sleep(3)
+    print("One in ten")
+    sleep(3)
+    print("Don't want to be your monkey wrench")
+    sleep(3)
+    print("One more indecent accident")
+    sleep(3)
+    print("I'd rather leave than suffer this")
+    sleep(3)
+    print("I'll never be your monkey wrench")
+    sleep(3)
+
+
+what()
---
+++ turtle.square.py
@@ -0,0 +1,7 @@
+import turtle
+
+for i in range(0,9):
+    turtle.forward(100)
+    turtle.right(140)
+
+turtle.exitonclick()
